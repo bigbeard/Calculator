782c99640794ad61aa78f5b45f0b17833989fe2e
bigbeard <cliffe@bigbeard.co.uk>
2013-12-09 22:22:43 +0000

    Initial commit

--- Calculator/ViewController.h
+++ Calculator/ViewController.h
@@ -8,6 +8,53 @@
 
 #import <UIKit/UIKit.h>
 
+typedef enum operationTypes
+{
+    NOOPERATION,
+    PLUS,
+    MINUS,
+    MULTIPLY,
+    DIVIDE
+} operation;
+
+
 @interface ViewController : UIViewController
+@property (weak, nonatomic) IBOutlet UITextField *displayText;
+- (IBAction)zeroButton:(UIButton *)sender;
+- (IBAction)oneButton:(UIButton *)sender;
+- (IBAction)twoButton:(UIButton *)sender;
+- (IBAction)threeButton:(UIButton *)sender;
+- (IBAction)fourButton:(UIButton *)sender;
+- (IBAction)fiveButton:(UIButton *)sender;
+- (IBAction)sixButton:(UIButton *)sender;
+- (IBAction)sevenButton:(UIButton *)sender;
+- (IBAction)eightButton:(UIButton *)sender;
+- (IBAction)nineButton:(UIButton *)sender;
+- (IBAction)decimalButton:(UIButton *)sender;
+
+
+- (IBAction)clearButton:(UIButton *)sender;
+- (IBAction)clearCurrentButton:(UIButton *)sender;
+
+- (IBAction)plusButtonClick:(UIButton *)sender;
+- (IBAction)equalsButtonClick:(UIButton *)sender;
+- (IBAction)minusButtonClick:(UIButton *)sender;
+- (IBAction)divideButtonClick:(UIButton *)sender;
+- (IBAction)multipleButtonClick:(UIButton *)sender;
+
+- (void) updateDisplay;
+- (void) appendToDisplay: (NSString *)text;
+- (void) operatorClicked: (operation *)operationClicked;
+- (void) calculateAndDisplayResult;
 
 @end
+
+NSMutableString *displayNumber;
+BOOL operatorClicked;
+operation lastOperation;
+BOOL lastButtonWasEquals;
+NSDecimalNumber *currentResult;
+NSDecimalNumber *currentValue;
+
+
+
--- Calculator/ViewController.m
+++ Calculator/ViewController.m
@@ -18,12 +18,152 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    displayNumber = [[NSMutableString alloc]initWithString: @"0"];
+    self.displayText.text = displayNumber;
+    currentResult = [NSDecimalNumber zero];
+    currentValue = [NSDecimalNumber zero];
+    lastOperation = NOOPERATION;
+    operatorClicked = true;
+    lastButtonWasEquals = false;
 }
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (IBAction)equalsButtonClick:(UIButton *)sender {
+    if (!lastButtonWasEquals) {
+        currentValue = [NSDecimalNumber decimalNumberWithString: self.displayText.text];
+    }
+    
+    [self calculateAndDisplayResult];
+    operatorClicked = true;
+    lastButtonWasEquals = true;
+}
+
+- (IBAction)plusButtonClick:(UIButton *)sender {
+    operation operator = PLUS;
+    [self operatorClicked: &operator];
+}
+
+- (IBAction)minusButtonClick:(UIButton *)sender {
+    operation operator = MINUS;
+    [self operatorClicked: &operator];
+}
+
+- (IBAction)divideButtonClick:(UIButton *)sender {
+    operation operator = DIVIDE;
+    [self operatorClicked: &operator];
+}
+
+- (IBAction)multipleButtonClick:(UIButton *)sender {
+    operation operator = MULTIPLY;
+    [self operatorClicked: &operator];
+}
+
+- (IBAction)zeroButton:(UIButton *)sender {
+    [self appendToDisplay: @"0"];
+}
+
+- (IBAction)oneButton:(UIButton *)sender {
+    [self appendToDisplay: @"1"];
+}
+
+- (IBAction)twoButton:(UIButton *)sender {
+    [self appendToDisplay: @"2"];
+}
+
+- (IBAction)threeButton:(UIButton *)sender {
+    [self appendToDisplay: @"3"];
+}
+
+- (IBAction)fourButton:(UIButton *)sender {
+    [self appendToDisplay: @"4"];
+}
+
+- (IBAction)fiveButton:(UIButton *)sender {
+    [self appendToDisplay: @"5"];
+}
+
+- (IBAction)sixButton:(UIButton *)sender {
+    [self appendToDisplay: @"6"];
+}
+
+- (IBAction)sevenButton:(UIButton *)sender {
+    [self appendToDisplay: @"7"];
+}
+
+- (IBAction)eightButton:(UIButton *)sender {
+    [self appendToDisplay: @"8"];
+}
+
+- (IBAction)nineButton:(UIButton *)sender {
+    [self appendToDisplay: @"9"];
+}
+
+- (IBAction)decimalButton:(UIButton *)sender {
+    if ([self.displayText.text rangeOfString: @"."].location == NSNotFound) {
+        [self appendToDisplay: @"."];
+    }
+}
+
+- (IBAction)clearButton:(UIButton *)sender {
+    [displayNumber setString: @"0"];
+    [self updateDisplay];
+    currentResult = [NSDecimalNumber zero];
+    lastOperation = NOOPERATION;
+    operatorClicked = true;
+}
+
+- (IBAction)clearCurrentButton:(UIButton *)sender {
+    [displayNumber setString: @""];
+    [self updateDisplay];
+}
+
+- (void) operatorClicked: (operation *) operationClicked {
+    if ((lastOperation != NOOPERATION) && (!lastButtonWasEquals)) {
+        currentValue = [NSDecimalNumber decimalNumberWithString: self.displayText.text];
+        [self calculateAndDisplayResult];
+    }
+    
+    operatorClicked = true;
+    lastOperation = *operationClicked;
+    currentResult = [NSDecimalNumber decimalNumberWithString: self.displayText.text];
+    lastButtonWasEquals = false;
+}
+
+- (void) appendToDisplay: (NSString *)text {
+    if (operatorClicked) {
+        [displayNumber setString: @""];
+        operatorClicked = false;
+    }
+    
+    [displayNumber appendString: text];
+    [self updateDisplay];
+    lastButtonWasEquals = false;
+}
+
+- (void) updateDisplay {
+    self.displayText.text = displayNumber;
+}
+
+- (void) calculateAndDisplayResult {
+    NSDecimalNumber *result = [NSDecimalNumber zero];
+    
+    if (lastOperation == PLUS) {
+        result = [currentResult decimalNumberByAdding: currentValue];
+    } else if (lastOperation == MINUS) {
+        result = [currentResult decimalNumberBySubtracting: currentValue];
+    } else if (lastOperation == MULTIPLY) {
+        result = [currentResult decimalNumberByMultiplyingBy: currentValue];
+    } else if (lastOperation == DIVIDE) {
+        result = [currentResult decimalNumberByDividingBy: currentValue];
+    }
+    
+    self.displayText.text = [result stringValue];
+    currentResult = [NSDecimalNumber decimalNumberWithDecimal: [result decimalValue]];
 }
 
 @end
